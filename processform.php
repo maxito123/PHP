0e6e09438f334e907d4614a5aeb67d79f77658fe
maxito123 <belovedmaximus@gmail.com>
2020-04-10 15:39:38 +0100

    first commit

---
+++ processform.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+if(isset($_POST['submit'])){
+
+    $firstName = $_POST['first_name'];
+
+    $lastName = $_POST['last_name'];
+
+    $email = $_POST['email'];
+
+    $gender= $_POST['gender'];
+
+    $message = $_POST['message'];
+
+
+
+    $file = fopen("$firstName.txt" , "a");
+
+    fwrite($file, $firstName);
+
+    fwrite($file, $lastName);
+
+    fwrite($file, $email);
+
+    fwrite($file, $gender);
+
+    fwrite($file, $message);
+}
